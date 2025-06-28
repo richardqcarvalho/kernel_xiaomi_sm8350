ac80e102e233e15be2446a2011d04cecb7666a6f
Cosmin Tanislav <demonsingur@gmail.com>
2025-06-28 19:28:00 +0200

    drivers: expose double tap node

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -3296,6 +3296,22 @@ static ssize_t fts_touchgame_store(struct device *dev,
 }
 #endif
 
+#ifdef GESTURE_MODE
+static ssize_t fts_double_tap_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d\n", fts_info->gesture_enabled);
+}
+
+static ssize_t fts_double_tap_store(struct device *dev, struct device_attribute *attr,
+				    const char *buf, size_t count)
+{
+	struct fts_ts_info *info = dev_get_drvdata(dev);
+	sscanf(buf, "%u", &info->gesture_enabled);
+	queue_work(info->event_wq, &info->mode_handler_work);
+	return count;
+}
+#endif
+
 static ssize_t fts_fod_area_show(struct device *dev,
 				    struct device_attribute *attr, char *buf)
 {
@@ -3576,6 +3592,12 @@ static DEVICE_ATTR(grip_enable, (S_IRUGO | S_IWUSR | S_IWGRP),
 		   fts_grip_enable_show, fts_grip_enable_store);
 static DEVICE_ATTR(grip_area, (S_IRUGO | S_IWUSR | S_IWGRP),
 		   fts_grip_area_show, fts_grip_area_store);
+
+#ifdef GESTURE_MODE
+
+static DEVICE_ATTR(double_tap, (S_IRUGO | S_IWUSR | S_IWGRP),
+		   fts_double_tap_show, fts_double_tap_store);
+#endif
 
 static DEVICE_ATTR(hover_tune, (S_IRUGO | S_IWUSR | S_IWGRP), NULL, fts_hover_autotune_store);
 
@@ -8428,6 +8450,14 @@ static int fts_probe(struct spi_device *client)
 	dev_set_drvdata(info->fts_touch_dev, info);
 #ifdef FTS_FOD_AREA_REPORT
 	mutex_init(&(info->fod_mutex));
+#endif
+#ifdef GESTURE_MODE
+	error =
+	    sysfs_create_file(&info->fts_touch_dev->kobj,
+		&dev_attr_double_tap.attr);
+	if (error) {
+		logError(1, "%s ERROR: Failed to create double_tap sysfs group!\n", tag);
+	}
 #endif
 	error =
 	    sysfs_create_file(&info->fts_touch_dev->kobj,
